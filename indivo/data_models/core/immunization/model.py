84d85e96807555836a2c8d8c67095226383d8468
James G. Kim <jgkim@jayg.org>
2014-07-07 18:42:37 +0900

    Tidied up the code regarding `Immunization` model and added `notes` field to it

--- indivo/data_models/core/immunization/model.py
+++ indivo/data_models/core/immunization/model.py
@@ -4,8 +4,9 @@ from indivo.fields import CodedValueField
 
 class Immunization(Fact):
     date = models.DateTimeField(null=True)
-    administration_status = CodedValueField()
+    product_name = CodedValueField()
     product_class = CodedValueField()
     product_class_2 = CodedValueField()
-    product_name = CodedValueField()
+    administration_status = CodedValueField()
     refusal_reason = CodedValueField()
+    notes = models.CharField(max_length=600, null=True)
